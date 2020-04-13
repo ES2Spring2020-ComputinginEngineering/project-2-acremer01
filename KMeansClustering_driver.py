4bc0fc2b9d85ff8874eb083dbc2b1b915e097513
acremer01 <allisoncremer01@gmail.com>
2020-04-12 21:01:51 -0600

    finish code

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -8,18 +8,22 @@ import KMeansClustering_functions as kmc
 import NearestNeighborClassification as nnc
 import numpy as np 
 
-
 # MAIN SCRIPT
 glucose, hemoglobin, humanclassification=kmc.openckdfile()
 glucose, hemoglobin, humanclassification=nnc.normalizeData(glucose, hemoglobin, humanclassification)
+# creates first centroids
 centroids=kmc.random_centroid_points(2)
+# creates first classification array
 oldclassifications=kmc.classification_array(centroids, glucose, hemoglobin)
+# updates centroids and classifications
 newcentroids=kmc.update_centroids(centroids, oldclassifications, glucose, hemoglobin)
 newclassifications=kmc.classification_array(newcentroids, glucose, hemoglobin)
+# while end condition is not met, continues updating values
 while kmc.determine_end_point(oldclassifications, newclassifications)==False:
     oldclassifications=newclassifications
     newcentroids=kmc.update_centroids(centroids, oldclassifications, glucose, hemoglobin)
     newclassifications=kmc.classification_array(newcentroids, glucose, hemoglobin)
+# prints and graphs values
 print(newclassifications)
 print(newcentroids)
 kmc.graphing_k_means(glucose, hemoglobin, newclassifications, newcentroids)
